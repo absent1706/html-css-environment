5fb5e4a99b8ac1283659805983f1a892bd5655f6
Alexander Litvinenko <litvinenko1706@gmail.com>
2017-08-13 23:53:08 +0300

    + use faker

--- gulpfile.js
+++ gulpfile.js
@@ -8,6 +8,7 @@ var notify = require("gulp-notify");
 var lazypipe = require("lazypipe");
 var runSequence = require('run-sequence');
 var nunjucksRender = require('gulp-nunjucks-render');
+var faker = require('faker');
 
 var notifyFileProcessedOptions = {
     sound: false,
@@ -25,9 +26,15 @@ var TEMPLATES_DIR = 'src/templates';
 var nunjucksOptions = {
     path: TEMPLATES_DIR,
     data: {
-        // var: 'value'
+        f: faker,
+        randint: (min,max) => {
+        	var range = max - min;
+        	var rand = Math.floor(Math.random() * (range + 1));
+        	return min + rand;
+        }
     }
 };
+nunjucksRender.nunjucks.installJinjaCompat();
 
 /* reusable pipe. note that we all pipes are functions that return, e.g. gulp.dest() */
 var processNunjucks = lazypipe()
@@ -40,7 +47,7 @@ gulp.task('build-css', function(){
         .pipe(sass().on('error', notifyError))
         .pipe(gulp.dest('dist/css'))
         .pipe(notify(notifyFileProcessedOptions))
-        .pipe(browserSync.stream({match: '**/*.css'}))
+        .pipe(browserSync.stream())
 });
 
 gulp.task('build-html', function() {
